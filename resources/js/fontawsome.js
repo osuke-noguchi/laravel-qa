7f9ba3e9adfb856ec730e8bc84b143905149c15e
oseke-noguchi <noguchi.nodai@gmail.com>
2019-11-02 14:54:28 +0900

    Add vote contrls to question and answer item

---
+++ resources/js/fontawsome.js
@@ -0,0 +1,16 @@
+// import fontawesome from '@fortawesome/fontawesome-free';
+
+// fontawesome.library.add([faCaretUp, faCaretDown, faStar, faCheck]);
+
+import { config, library, dom } from '@fortawesome/fontawesome-svg-core';
+config.autoReplaceSvg = 'nest';
+import { faCaretUp,
+ faCaretDown,
+ faStar,
+ faCheck } from '@fortawesome/free-solid-svg-icons';
+library.add(faCaretUp,
+ faCaretDown,
+ faStar,
+ faCheck);
+// Kicks off the process of finding <i> tags and replacing with <svg>
+dom.watch();
