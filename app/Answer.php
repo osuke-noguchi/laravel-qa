b5b7b18836f9adc1c2a2ec0886593343400ac079
oseke-noguchi <noguchi.nodai@gmail.com>
2019-11-12 23:16:00 +0900

    Update best_answer_id on questions table when answer removed in database layer

--- app/Answer.php
+++ app/Answer.php
@@ -31,12 +31,7 @@ class Answer extends Model
         });
 
         static::deleted(function ($answer) {
-          $question = $answer->question;
-          $question->decrement('answers_count');
-          if ($question->best_answer_id === $answer->id) {
-              $question->best_answer_id = NULL;
-              $question->save();
-          }
+          $answer->question->decrement('answers_count');
         });
     }
 
@@ -45,7 +40,7 @@ class Answer extends Model
         return $this->created_at->diffForHumans();
     }
 
-    public function getStatusAttribute() 
+    public function getStatusAttribute()
     {
         return $this->id === $this->question->best_answer_id ? 'vote-accepted' : '';
     }
