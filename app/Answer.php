6b9a2b967923d3450a5c048a19c9fdf4795d4d7d
oseke-noguchi <noguchi.nodai@gmail.com>
2019-11-25 22:50:06 +0900

    Implement many to many polymorhpic relationship and make the vote quesiton controls working

--- app/Answer.php
+++ app/Answer.php
@@ -54,4 +54,9 @@ class Answer extends Model
     {
         return $this->id === $this->question->best_answer_id;
     }
+
+    public function votes()
+    {
+        return $this->morphToMan(User::class, 'votable');
+    }
 }
